d5f363644af4811960eb00c8ad1eba9b623f6875
muradpm <abdulkadyr0v@yandex.ru>
2026-02-15 23:37:12 +0300

    build: add shared next config package

---
+++ packages/next-config/keys.ts
@@ -0,0 +1,29 @@
+import { vercel } from "@t3-oss/env-core/presets-zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod/v3";
+
+export const keys = () =>
+  createEnv({
+    extends: [vercel()],
+    server: {
+      ANALYZE: z.string().optional(),
+
+      // Added by Vercel
+      NEXT_RUNTIME: z.enum(["nodejs", "edge"]).optional(),
+
+      // URLs
+      FLOWNI_WEB_URL: z.string().url().min(1),
+      FLOWNI_API_URL: z.string().url().min(1),
+
+      // Node
+      NODE_ENV: z.enum(["development", "production", "test"]).optional(),
+    },
+    client: {},
+    runtimeEnv: {
+      ANALYZE: process.env.ANALYZE,
+      NEXT_RUNTIME: process.env.NEXT_RUNTIME,
+      FLOWNI_WEB_URL: process.env.FLOWNI_WEB_URL,
+      FLOWNI_API_URL: process.env.FLOWNI_API_URL,
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  });
